d2c0381b6633b91c8e03bfe03fbd09729dca0fb6
Shreeniti7013 <Kirtana.bhattarai@gmail.com>
2025-12-09 19:42:24 +0545

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,7 @@
+#Symetric diffrence between two sets;shows all in result except common elements
+
+set_A={1,2,3,4,5}
+set_B={5,3,8,6,2}
+result=set_A.symmetric_diffrence(set_B)
+print=("Symmetric Diffrence:",result)
+print("\n")
